09e2e0d7511ff9ebc5efa6300a9bc2d8a69190c1
PrasuHirapara <prasuhirpara@gmail.com>
2025-12-01 01:57:27 +0530

    adding indian time zone

--- backend/src/main/java/com/canaristar/backend/BackendApplication.java
+++ backend/src/main/java/com/canaristar/backend/BackendApplication.java
@@ -2,10 +2,13 @@ package com.canaristar.backend;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import io.github.cdimascio.dotenv.DotenvEntry;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
@@ -28,4 +31,10 @@ public class BackendApplication {
 
         SpringApplication.run(BackendApplication.class, args);
     }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+    }
+
 }
--- backend/src/main/java/com/canaristar/backend/controller/GeneralController.java
+++ backend/src/main/java/com/canaristar/backend/controller/GeneralController.java
@@ -6,22 +6,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-
 @RestController
 public class GeneralController {
 
     @GetMapping("/")
     public ResponseEntity<GeneralResponse> live() {
-        ZoneId indiaZone = ZoneId.of("Asia/Kolkata");
-        LocalDateTime indiaTime = LocalDateTime.now(indiaZone);
-        String formattedTime = indiaTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        GeneralResponse gr = new GeneralResponse("live");
 
-        GeneralResponse gr = new GeneralResponse("live at " + formattedTime);
         return new ResponseEntity<>(gr, HttpStatus.OK);
     }
-
-
 }
