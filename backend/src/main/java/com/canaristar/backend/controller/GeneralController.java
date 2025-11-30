72fdc42defcaa83e5aafce09252cf1288a120e6b
PrasuHirapara <prasuhirpara@gmail.com>
2025-12-01 01:43:32 +0530

    adding indian time zone

--- backend/src/main/java/com/canaristar/backend/controller/GeneralController.java
+++ backend/src/main/java/com/canaristar/backend/controller/GeneralController.java
@@ -6,13 +6,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 public class GeneralController {
 
     @GetMapping("/")
     public ResponseEntity<GeneralResponse> live() {
-        GeneralResponse gr = new GeneralResponse("live");
+        ZoneId indiaZone = ZoneId.of("Asia/Kolkata");
+        LocalDateTime indiaTime = LocalDateTime.now(indiaZone);
+        String formattedTime = indiaTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 
+        GeneralResponse gr = new GeneralResponse("live at " + formattedTime);
         return new ResponseEntity<>(gr, HttpStatus.OK);
     }
+
+
 }
